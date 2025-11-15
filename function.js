d0f39d03de6e0e47fa18e0b1f7217d0daf521302
Nikolozi11 <nikolkzgazviani2011@gmail.com>
2025-11-15 18:10:52 +0400

    project

---
+++ function.js
@@ -0,0 +1,21 @@
+// Magical sparkles
+for(let i=0;i<120;i++){
+  const sparkle=document.createElement('div');
+  sparkle.classList.add('sparkle');
+  sparkle.style.top=Math.random()*100+'%';
+  sparkle.style.left=Math.random()*100+'%';
+  sparkle.style.animationDuration=(Math.random()*3+2)+'s';
+  sparkle.style.width=2+Math.random()*4+'px';
+  sparkle.style.height=2+Math.random()*4+'px';
+  document.body.appendChild(sparkle);
+}// Magical sparkles
+for(let i=0;i<120;i++){
+  const sparkle=document.createElement('div');
+  sparkle.classList.add('sparkle');
+  sparkle.style.top=Math.random()*100+'%';
+  sparkle.style.left=Math.random()*100+'%';
+  sparkle.style.animationDuration=(Math.random()*3+2)+'s';
+  sparkle.style.width=2+Math.random()*4+'px';
+  sparkle.style.height=2+Math.random()*4+'px';
+  document.body.appendChild(sparkle);
+}
